06ea0ea4689704800224c55f0b4b0337a48fc4d7
JENNIFER LUNA <1320-20@itmlyceumalabang.onmicrosoft.com>
2022-02-07 17:25:56 +0800

    Created a FrameRepository

---
+++ infrastructure_project/Repositories/FrameRepository.cs
@@ -0,0 +1,41 @@
+﻿using domain_project.domain.AggregateModels.Game;
+using domain_project.domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure_project.Repositories
+{
+    class FrameRepository : IGameRepository
+    {
+        public IUnitOfWork UnitOfWork => throw new NotImplementedException();
+
+        public Task<Game> CreateAsync(Game entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Game> GetAsQueryable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> UpdateAsync(Game entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
